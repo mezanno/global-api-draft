9e0231c7cedcda0fb046fd160cdd13986999cbb4
Joseph Chazalon <joseph.chazalon@epita.fr>
2025-04-22 17:14:26 +0200

    api-ocr: update test script to new gradio api path

--- api-ocr/test_ocr.sh
+++ api-ocr/test_ocr.sh
@@ -11,5 +11,5 @@ SERVER_URL=https://api.mezanno.xyz/ocr
 (for ii in $(seq 1 $NUM); do
   echo '{"data":["https://cache.mezanno.xyz/openapi.bnf.fr/iiif/image/v3/ark:/12148/bd6t543045578/f100/full/max/0/default.webp","[]"]}'
 done ) | \
-    parallel "curl -X POST -Ss ${SERVER_URL}/call/${API_NAME} -H 'Content-Type: application/json' -d {} | jq -r .event_id" \
-  | time parallel --tag -j $NUM curl -X GET -Ss ${SERVER_URL}/call/${API_NAME}/{}
+    parallel "curl -X POST -Ss ${SERVER_URL}/gradio_api/call/${API_NAME} -H 'Content-Type: application/json' -d {} | jq -r .event_id" \
+  | time parallel --tag -j $NUM curl -X GET -Ss ${SERVER_URL}/gradio_api/call/${API_NAME}/{}
