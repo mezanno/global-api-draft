d7aa78e47c4bb468b70b927ae33cf4c14e9f59cd
Joseph Chazalon <joseph.chazalon@epita.fr>
2025-04-22 17:24:46 +0200

    deploy: improve tagging script

--- 02-docker_images_tag.sh
+++ 02-docker_images_tag.sh
@@ -10,3 +10,9 @@ SERVICES="api-gateway cache layout-worker-wrapper layout-worker api-ocr ocr-work
 for service in $SERVICES ; do
     docker image tag testfull-${service}:latest ${REGISTRY}/mezanno-${service}:v${TIMESTAMP}
 done;
+
+echo "Tagged images:"
+docker images | grep mezanno | grep v${TIMESTAMP}
+
+echo "You may need to export the TAG with the following command:"
+echo "export TAG=v${TIMESTAMP}"
